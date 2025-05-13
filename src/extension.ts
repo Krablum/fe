7d933143044d5e227664570199aeaaa4c04af839
Krab <kevinnamorabech@gmail.com>
2025-05-13 10:20:34 +0700

    creating the basic layout of scope.ts aswell as changing minor things

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,8 @@ import * as multiCurEx from './lib';
 
 export function activate(context: vscode.ExtensionContext) {	
 
+	console.log(vscode.SymbolInformation);
+
 	const mediaPath = (() : string =>{	
 
 		const path = (__dirname.split('\\'));
@@ -17,19 +19,24 @@ export function activate(context: vscode.ExtensionContext) {
 		 return path.join('\\') + '\\media\\origin.svg';
 	})();
 	
-	const disposablePadding = vscode.commands.registerCommand("multicursorex.padding.pad" ,  multiCurEx.padding.pad(mediaPath));	
-	const disposableOriginUp = vscode.commands.registerCommand("multicursorex.padding.originUp" , multiCurEx.padding.originUp(mediaPath));	
-	const disposableOriginDown = vscode.commands.registerCommand("multicursorex.padding.originDown" , multiCurEx.padding.originDown(mediaPath));	
-	const disposableShowOrigin = vscode.commands.registerCommand("multicursorex.padding.originShow" , multiCurEx.padding.showOrigin(mediaPath));
-	const disposableLockOriginToMedian =  vscode.commands.registerCommand("multicursorex.padding.lockOriginToMedian" , multiCurEx.padding.lockOriginToMedian(mediaPath));	
-	
-	context.subscriptions.push(disposablePadding, disposableOriginUp, disposableOriginDown, disposableShowOrigin, disposableLockOriginToMedian);
-	
+	const disposablePadding = vscode.commands.registerCommand("multicursorex.padding.pad" ,  multiCurEx.Padding.pad(mediaPath));	
+	const disposableOriginUp = vscode.commands.registerCommand("multicursorex.padding.originUp" , multiCurEx.Padding.originUp(mediaPath));	
+	const disposableOriginDown = vscode.commands.registerCommand("multicursorex.padding.originDown" , multiCurEx.Padding.originDown(mediaPath));	
+	const disposableShowOrigin = vscode.commands.registerCommand("multicursorex.padding.originShow" , multiCurEx.Padding.showOrigin(mediaPath));
+	const disposableLockOriginToMedian =  vscode.commands.registerCommand("multicursorex.padding.lockOriginToMedian" , multiCurEx.Padding.lockOriginToMedian(mediaPath));	
 
+	context.subscriptions.push(disposablePadding, disposableOriginUp, disposableOriginDown, disposableShowOrigin, disposableLockOriginToMedian);
+
+	const disposableScopeOn = vscode.commands.registerCommand("multicursorex.scope.on", multiCurEx.Scope.modes.on());
+	const disposableScopeOff = vscode.commands.registerCommand("multicursorex.scope.off", multiCurEx.Scope.modes.off());
+	const disposableScopeSwitch = vscode.commands.registerCommand("multicursorex.scope.switch", multiCurEx.Scope.modes.switch());
+
+	context.subscriptions.push(disposableScopeOn, disposableScopeOff, disposableScopeSwitch);
+	
 }
 
 export function deactivate() {
 
-	multiCurEx.padding.disposeAllDecoration();
+	multiCurEx.Padding.disposeAllDecoration();
 
 }
